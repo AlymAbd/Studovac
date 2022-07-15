d2fd41f16e0cc2faf2f8db9c3778876eec96644b
Alym <abd.alym@outlook.com>
2022-07-15 11:12:01 +0200

    update sematic ui

--- resources/vue/main.js
+++ resources/vue/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 
 import App from '@v/App.vue'
 import router from '@v/router'
-
+import SuiVue from 'semantic-ui-vue'
 import axios from 'axios'
+import 'semantic-ui-css/semantic.min.css'
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios
 app.use(router)
+app.use(SuiVue)
 app.mount('#app')
+
